fb0ece8b5e5d80b7d9ee08e866f2fe6b0c62de12
RobSorce <srcrbt@gmail.com>
2018-04-21 16:11:32 +0200

    Update homework5.cpp

--- homework5/src/homework5.cpp
+++ homework5/src/homework5.cpp
@@ -12,25 +12,24 @@
 #include <pcl/visualization/pcl_visualizer.h>
 #include <pcl/console/parse.h>
 #include <pcl_conversions/pcl_conversions.h>
+#include <pcl/visualization/point_cloud_color_handlers.h>
 
 using namespace pcl; //mi permette di accedere a tutti i campi e strutture pcl;
 
-pcl::VoxelGrid<pcl::PointXYZ> voxel;
-pcl::PassThrough<pcl::PointXYZ> pass;
-pcl::visualization::CloudViewer viewer("Cloud Viewer");
+//pcl::visualization::CloudViewer viewer("Cloud Viewer");
 
 
-void pclCallback(const sensor_msgs::PointCloud2::ConstPtr& pc){
+void pclCallback(const sensor_msgs::PointCloud2 pc){
 
-  PointCloud<pcl::PointXYZ>::Ptr cloud_pt (new pcl::PointCloud<pcl::PointXYZ>);
-  PointCloud<pcl::PointXYZ>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZ>);
-  PointCloud<pcl::PointXYZ>::Ptr cloud_voxelized (new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_pt (new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_voxelized (new pcl::PointCloud<pcl::PointXYZ>);
 
-  pcl::fromROSMsg(*pc, *cloud_pt);
+  pcl::fromROSMsg(pc, *cloud_pt);
 
   boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer_unfiltered(new pcl::visualization::PCLVisualizer ("3D Viewer"));
   viewer_unfiltered->setBackgroundColor (0, 0, 0);
-  viewer_unfiltered->addPointCloud<pcl::PointXYZ> (cloud_pt, "Input CLoud Unfiltered" );
+  viewer_unfiltered->addPointCloud<pcl::PointXYZ> (cloud_pt, "Input Cloud Unfiltered" );
   viewer_unfiltered->initCameraParameters();
   viewer_unfiltered->addCoordinateSystem(1.0);
   viewer_unfiltered->spin();
@@ -38,6 +37,7 @@ void pclCallback(const sensor_msgs::PointCloud2::ConstPtr& pc){
 
 
   // Create the filtering object
+  pcl::PassThrough<pcl::PointXYZ> pass;
   pass.setInputCloud (cloud_pt);
   pass.setFilterFieldName ("z");
   pass.setFilterLimits (0.0, 1.0);
@@ -46,11 +46,18 @@ void pclCallback(const sensor_msgs::PointCloud2::ConstPtr& pc){
 
 
   //Voxelize the filtered cloud;
+  pcl::VoxelGrid<pcl::PointXYZ> voxel;
   voxel.setInputCloud (cloud_filtered);
   voxel.setLeafSize (0.01, 0.01, 0.01);
   voxel.filter (*cloud_voxelized);
 
-  viewer.showCloud(cloud_voxelized);
+  //viewer.showCloud(cloud_voxelized);
+  boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer_filtered (new pcl::visualization::PCLVisualizer ("3D Viewer"));
+  viewer_filtered->setBackgroundColor (0, 0, 0);
+  viewer_filtered->addPointCloud<pcl::PointXYZ> (cloud_voxelized, "Filtered" );
+  viewer_filtered->initCameraParameters ();
+  viewer_filtered->addCoordinateSystem (1.0);
+  viewer_filtered->spin();
 }
 
 int main(int argc, char **argv){
